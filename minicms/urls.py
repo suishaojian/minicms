c150ddcabba14b7c3720ddd51ba63ab06d277134
suishaojian <daniel@finstone>
2016-10-14 16:39:47 +0800

    内容管理系统

--- minicms/urls.py
+++ minicms/urls.py
@@ -17,8 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from DjangoUeditor import urls as DjangoUeditor_urls
 from django.conf import settings
-
+from news import views
 urlpatterns = [
+
+    url(r'^$',views.index,name='index'),
+    url(r'^column/(?P<column_slug>[^/]+)/$', views.column_detail, name='column'),
+    url(r'^news/(?P<article_slug>[^/]+)/$', views.article_detail, name='article'),
+
     url(r'^admin/', admin.site.urls),
     url(r'^ueditor/',DjangoUeditor_urls.get_ueditor_controller),
 ]
