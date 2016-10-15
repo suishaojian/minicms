c9e8fea554d35e78545c873cd6c45c5f9d635cb1
suishaojian <daniel@finstone>
2016-10-15 14:22:46 +0800

    201610151350

--- news/views.py
+++ news/views.py
@@ -1,13 +1,18 @@
 #encoding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Column
+from models import Article
 # Create your views here.
 
 def index(request):
-    return HttpResponse(u'Welcome Daniel')
+    columns = Column.objects.all()
+    return render(request, 'index.html', {'columns': columns})
 
 def column_detail(request,column_slug):
-    return HttpResponse('column_slug: '+column_slug)
+    column = Column.objects.get(slug=column_slug)
+    return render(request,'news/column.html',{'column':column})
 
-def article_detail(request,article_slug):
-    return HttpResponse('article_slug: '+article_slug)
+def article_detail(request, article_slug):
+    article = Article.objects.filter(slug=article_slug)[0]
+    return render(request, 'news/article.html', {'article': article})
